86cc629d1f36c41073eeb26568cf0615e35733bc
dennis-tech <irungudennis956@yahoo.com>
2022-03-28 04:18:21 +0300

    added object player with name and number of the players chips and rendered it to DOM

--- Js/index.js
+++ Js/index.js
@@ -8,6 +8,12 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 
+let player = {
+    name: "Dennis",
+    chips: 200
+}
+let playerEl = document.getElementById("player-el");
+playerEl.textContent += player.name + " : $" + player.chips;
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
